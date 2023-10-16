c4bcf90a6250978bb440c9762ac227925d318e17
Harsh <hb118@students.waikato.ac.nz>
2023-10-17 06:51:04 +1300

    Added in proper styling and adding logic behind selecting exercises.

--- master.ino
+++ master.ino
@@ -1,8 +1,3 @@
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com  
-*********/
-
 // Import required libraries
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
@@ -15,6 +10,7 @@ const char* password = "password";
 String title;
 String desc;
 int exerciseNum = 0;
+const String footballSymbol = "&#9917";
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -41,6 +37,42 @@ String processor(const String& var){
   if(var == "DESC"){
     return desc;
   }
+  if(var == "FOOTBALL1"){
+    if(exerciseNum == 1){
+      return footballSymbol;
+    } else{
+      return "";
+    }
+  }
+  if(var == "FOOTBALL2"){
+    if(exerciseNum == 2){
+      return footballSymbol;
+    } else{
+      return "";
+    }
+  }
+  if(var == "FOOTBALL3"){
+    if(exerciseNum == 3){
+      return footballSymbol;
+    } else{
+      return "";
+    }
+  }
+  if(var == "FOOTBALL4"){
+    if(exerciseNum == 4){
+      return footballSymbol;
+    } else{
+      return "";
+    }
+  }
+  if(var == "FOOTBALL5"){
+    if(exerciseNum == 5){
+      return footballSymbol;
+    } else{
+      return "";
+    }
+  }
+  
   return String();
 }
  
@@ -82,6 +114,31 @@ void setup(){
   });
 
   server.on("/connect.html", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/connect.html", String(), false, processor);
+  });
+
+  server.on("/one", HTTP_GET, [](AsyncWebServerRequest *request){
+    exerciseNum = 1;
+    request->send(SPIFFS, "/connect.html", String(), false, processor);
+  });
+
+  server.on("/two", HTTP_GET, [](AsyncWebServerRequest *request){
+    exerciseNum = 2;
+    request->send(SPIFFS, "/connect.html", String(), false, processor);
+  });
+
+  server.on("/three", HTTP_GET, [](AsyncWebServerRequest *request){
+    exerciseNum = 3;
+    request->send(SPIFFS, "/connect.html", String(), false, processor);
+  });
+
+  server.on("/four", HTTP_GET, [](AsyncWebServerRequest *request){
+    exerciseNum = 4;
+    request->send(SPIFFS, "/connect.html", String(), false, processor);
+  });
+
+  server.on("/five", HTTP_GET, [](AsyncWebServerRequest *request){
+    exerciseNum = 5;
     request->send(SPIFFS, "/connect.html", String(), false, processor);
   });
 
