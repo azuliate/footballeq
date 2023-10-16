f41f3aabe6e19b55430a56b047b515f1f2765040
Harsh <hb118@students.waikato.ac.nz>
2023-10-17 11:03:55 +1300

    Added in .json parsing. Added in Mesh Network capabilities.

--- master.ino
+++ master.ino
@@ -2,6 +2,22 @@
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
 #include "SPIFFS.h"
+#include <esp_now.h>
+#include <ArduinoJson.h>
+
+typedef struct FeqData{
+  int node;
+  char phase1_colour[2];
+  int phase1_seconds;
+  char phase2_colour[2];
+  int phase2_seconds;
+  char phase3_colour[2];
+  int phase3_seconds;
+  char phase4_colour[2];
+  int phase4_seconds;
+} FeqData;
+
+FeqData dataNode1;
 
 // Replace with your network credentials
 const char* ssid = "FOOTBALLEYEQ";
@@ -9,11 +25,26 @@ const char* password = "password";
 
 String title;
 String desc;
-int exerciseNum = 0;
+int exerciseNum;
 const String footballSymbol = "&#9917";
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
+
+esp_now_peer_info_t peerInfo;
+
+uint8_t node1[] = {0x34, 0x85, 0x18, 0x75, 0xE1, 0x24};
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+    char macStr[18];
+  Serial.print("Packet to: ");
+  // Copies the sender mac address to a string
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print(macStr);
+  Serial.print(" send status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
 
 void evaluateExercise(){
   switch(exerciseNum){
@@ -23,11 +54,135 @@ void evaluateExercise(){
       break;
     case 1:
       title = "Exercise 1";
-      desc = "Exercise 1 Description =D";
+      desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+      break;
+    case 2:
+      title = "Exercise 2";
+      desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+      break;
+    case 3:
+      title = "Exercise 3";
+      desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+      break;
+    case 4:
+      title = "Exercise 4";
+      desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+      break;
+    case 5:
+      title = "Exercise 5";
+      desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+      break;
     default:
       break;
   }
 }
+
+void parseExercise(){
+  if(exerciseNum == 1){
+    File exerciseFile = SPIFFS.open("/exercises/exercise1.json", "r");
+      if(!exerciseFile){
+        Serial.println("Failed to open config file");
+        return;
+      }
+
+      size_t size = exerciseFile.size();
+
+      //Allocate buffer to store json contents
+      std::unique_ptr<char[]> buf(new char[size]);
+
+      exerciseFile.readBytes(buf.get(), size);
+
+      const size_t capacity = 16*JSON_ARRAY_SIZE(1) + 5*JSON_ARRAY_SIZE(4) + 17*JSON_OBJECT_SIZE(1) + 20*JSON_OBJECT_SIZE(2) + 590;
+      DynamicJsonBuffer jsonBuffer(capacity);
+      JsonObject& json = jsonBuffer.parseObject(buf.get());
+
+      if(!json.success()){
+        Serial.println("Failed to parse exercise file.");
+        return;
+      }
+
+      dataNode1.node = json["nodes"][0]["node"]; // 1
+
+  
+
+      const char* node1_phase1_colour = json["nodes"][0]["phases"][0]["phase1"][0]["colour"];
+      dataNode1.phase1_colour[0] = node1_phase1_colour[0];
+      dataNode1.phase1_colour[1] = '\0';
+      int node1_phase1_seconds = json["nodes"][0]["phases"][0]["phase1"][0]["seconds"];
+      dataNode1.phase1_seconds = node1_phase1_seconds;
+
+      const char* node1_phase2_colour = json["nodes"][0]["phases"][1]["phase2"][0]["colour"];
+      dataNode1.phase2_colour[0] = node1_phase2_colour[0];
+      dataNode1.phase2_colour[1] = '\0';
+      int node1_phase2_seconds = json["nodes"][0]["phases"][1]["phase2"][0]["seconds"];
+      dataNode1.phase2_seconds = node1_phase2_seconds;
+
+      const char* node1_phase3_colour = json["nodes"][0]["phases"][2]["phase3"][0]["colour"];
+      dataNode1.phase3_colour[0] = node1_phase3_colour[0];
+      dataNode1.phase3_colour[1] = '\0';
+      int node1_phase3_seconds = json["nodes"][0]["phases"][2]["phase3"][0]["seconds"];
+      dataNode1.phase3_seconds = node1_phase3_seconds;
+
+      const char* node1_phase4_colour = json["nodes"][0]["phases"][3]["phase4"][0]["colour"];
+      dataNode1.phase4_colour[0] = node1_phase4_colour[0];
+      dataNode1.phase4_colour[1] = '\0';
+      int node1_phase4_seconds = json["nodes"][0]["phases"][3]["phase4"][0]["seconds"];
+      dataNode1.phase4_seconds = node1_phase4_seconds;
+      exerciseFile.close();
+      return;
+      } else{
+        Serial.println("No exercise selected.");
+      }
+}
+  
+
+  /*int nodes_1_node = nodes[1]["node"]; // 2
+
+  JsonArray& nodes_1_phases = nodes[1]["phases"];
+
+  const char* nodes_1_phases_0_phase1_0_colour = nodes_1_phases[0]["phase1"][0]["colour"]; // "Red"
+  int nodes_1_phases_0_phase1_0_seconds = nodes_1_phases[0]["phase1"][0]["seconds"]; // 10
+
+  const char* nodes_1_phases_1_phase2_0_colour = nodes_1_phases[1]["phase2"][0]["colour"]; // "Green"
+  int nodes_1_phases_1_phase2_0_seconds = nodes_1_phases[1]["phase2"][0]["seconds"]; // 5
+
+  const char* nodes_1_phases_2_phase3_0_colour = nodes_1_phases[2]["phase3"][0]["colour"]; // "Red"
+  int nodes_1_phases_2_phase3_0_seconds = nodes_1_phases[2]["phase3"][0]["seconds"]; // 5
+
+  const char* nodes_1_phases_3_phase4_0_colour = nodes_1_phases[3]["phase4"][0]["colour"]; // "Green"
+  int nodes_1_phases_3_phase4_0_seconds = nodes_1_phases[3]["phase4"][0]["seconds"]; // 5
+
+  int nodes_2_node = nodes[2]["node"]; // 3
+
+  JsonArray& nodes_2_phases = nodes[2]["phases"];
+
+  const char* nodes_2_phases_0_phase1_0_colour = nodes_2_phases[0]["phase1"][0]["colour"]; // "Green"
+  int nodes_2_phases_0_phase1_0_seconds = nodes_2_phases[0]["phase1"][0]["seconds"]; // 5
+
+  const char* nodes_2_phases_1_phase2_0_colour = nodes_2_phases[1]["phase2"][0]["colour"]; // "Red"
+  int nodes_2_phases_1_phase2_0_seconds = nodes_2_phases[1]["phase2"][0]["seconds"]; // 5
+
+  const char* nodes_2_phases_2_phase3_0_colour = nodes_2_phases[2]["phase3"][0]["colour"]; // "Green"
+  int nodes_2_phases_2_phase3_0_seconds = nodes_2_phases[2]["phase3"][0]["seconds"]; // 10
+
+  const char* nodes_2_phases_3_phase4_0_colour = nodes_2_phases[3]["phase4"][0]["colour"]; // "Red"
+  int nodes_2_phases_3_phase4_0_seconds = nodes_2_phases[3]["phase4"][0]["seconds"]; // 5
+
+  int nodes_3_node = nodes[3]["node"]; // 4
+
+  JsonArray& nodes_3_phases = nodes[3]["phases"];
+
+  const char* nodes_3_phases_0_phase1_0_colour = nodes_3_phases[0]["phase1"][0]["colour"]; // "Green"
+  int nodes_3_phases_0_phase1_0_seconds = nodes_3_phases[0]["phase1"][0]["seconds"]; // 5
+
+  const char* nodes_3_phases_1_phase2_0_colour = nodes_3_phases[1]["phase2"][0]["colour"]; // "Red"
+  int nodes_3_phases_1_phase2_0_seconds = nodes_3_phases[1]["phase2"][0]["seconds"]; // 5
+
+  const char* nodes_3_phases_2_phase3_0_colour = nodes_3_phases[2]["phase3"][0]["colour"]; // "Green"
+  int nodes_3_phases_2_phase3_0_seconds = nodes_3_phases[2]["phase3"][0]["seconds"]; // 10
+
+  const char* nodes_3_phases_3_phase4_0_colour = nodes_3_phases[3]["phase4"][0]["colour"]; // "Red"
+  int nodes_3_phases_3_phase4_0_seconds = nodes_3_phases[3]["phase4"][0]["seconds"]; // 5 */
 
 String processor(const String& var){
   evaluateExercise();
@@ -95,6 +250,33 @@ void setup(){
   Serial.print("AP IP address: ");
   Serial.println(IP);
 
+  //initialize ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  //register callback function
+  esp_now_register_send_cb(OnDataSent);
+
+  //register peer
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  //register first peer
+  memcpy(peerInfo.peer_addr, node1, 6);
+   if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer 1");
+  }
+  // register second peer  
+  /*memcpy(peerInfo.peer_addr, node2, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer 2");
+  }/*
+  /// register third peer
+  /*memcpy(peerInfo.peer_addr, node3, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer 3");
+  }*/
   
 
   //set wifi mode
@@ -105,41 +287,54 @@ void setup(){
 
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html", "text/html");
+    exerciseNum = 0;
+    request->send(SPIFFS, "/webserver/index.html", "text/html");
   });
   
   // Route to load style.css file
   server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/style.css", "text/css");
+    request->send(SPIFFS, "/webserver/style.css", "text/css");
   });
 
   server.on("/connect.html", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/connect.html", String(), false, processor);
+    request->send(SPIFFS, "/webserver/connect.html", String(), false, processor);
   });
 
   server.on("/one", HTTP_GET, [](AsyncWebServerRequest *request){
     exerciseNum = 1;
-    request->send(SPIFFS, "/connect.html", String(), false, processor);
+    parseExercise();
+    request->send(SPIFFS, "/webserver/connect.html", String(), false, processor);
   });
 
   server.on("/two", HTTP_GET, [](AsyncWebServerRequest *request){
     exerciseNum = 2;
-    request->send(SPIFFS, "/connect.html", String(), false, processor);
+    request->send(SPIFFS, "/webserver/connect.html", String(), false, processor);
   });
 
   server.on("/three", HTTP_GET, [](AsyncWebServerRequest *request){
     exerciseNum = 3;
-    request->send(SPIFFS, "/connect.html", String(), false, processor);
+    request->send(SPIFFS, "/webserver/connect.html", String(), false, processor);
   });
 
   server.on("/four", HTTP_GET, [](AsyncWebServerRequest *request){
     exerciseNum = 4;
-    request->send(SPIFFS, "/connect.html", String(), false, processor);
+    request->send(SPIFFS, "/webserver/connect.html", String(), false, processor);
   });
 
   server.on("/five", HTTP_GET, [](AsyncWebServerRequest *request){
     exerciseNum = 5;
-    request->send(SPIFFS, "/connect.html", String(), false, processor);
+    request->send(SPIFFS, "/webserver/connect.html", String(), false, processor);
+  });
+
+  server.on("/start", HTTP_GET, [](AsyncWebServerRequest *request){
+    esp_err_t result = esp_now_send(node1, (uint8_t *) &dataNode1, sizeof(FeqData));
+      if (result == ESP_OK) {
+        Serial.println("Sent with success");
+      }
+      else {
+        Serial.println("Error sending the data");
+      }
+    request->send(SPIFFS, "/webserver/connect.html", String(), false, processor);
   });
 
   // Start server
---
+++ worker/worker.ino
@@ -0,0 +1,72 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+typedef struct exercise {
+  int node;
+  char phase1_colour[2];
+  int phase1_seconds;
+  char phase2_colour[2];
+  int phase2_seconds;
+  char phase3_colour[2];
+  int phase3_seconds;
+  char phase4_colour[2];
+  int phase4_seconds;
+} exercise;
+
+
+exercise myData;
+
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("nodeNum: ");
+  Serial.println(myData.node);
+
+  Serial.print("Phase 1 Colour: ");
+  Serial.print(myData.phase1_colour);
+  Serial.println();
+  Serial.print("Phase 1 Seconds: ");
+  Serial.println(myData.phase1_seconds);
+
+  Serial.print("Phase 2 Colour: ");
+  Serial.print(myData.phase2_colour);
+  Serial.println();
+  Serial.print("Phase 2 Seconds: ");
+  Serial.println(myData.phase2_seconds);
+
+  Serial.print("Phase 3 Colour: ");
+  Serial.print(myData.phase3_colour);
+  Serial.println();
+  Serial.print("Phase 3 Seconds: ");
+  Serial.println(myData.phase3_seconds);
+
+  Serial.print("Phase 4 Colour: ");
+  Serial.print(myData.phase4_colour);
+  Serial.println();
+  Serial.print("Phase 4 Seconds: ");
+  Serial.println(myData.phase4_seconds);
+
+  Serial.println();
+}
+
+void setup() {
+  //Initialize Serial Monitor
+  Serial.begin(115200);
+  
+  //Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  //Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+void loop() {
+
+
+}
